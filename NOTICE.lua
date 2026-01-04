4055777778ae3266e46ad053a4939bf0091d9c07
ImNotClippy <appus321321@outlook.com>
2026-01-04 22:43:38 +0000

    ssss
    ssssssssssss

---
+++ NOTICE.lua
@@ -0,0 +1,14 @@
+--[[--------------------------------------------------------
+ ____       _       ____                  
+ |  _ \ __ _(_)_ __ |  _ \ _ __ ___  _ __  
+ | |_) / _` | | '_ \| | | | '__/ _ \| '_ \ 
+ |  _ < (_| | | | | | |_| | | | (_) | |_) |
+ |_| \_\__,_|_|_| |_|____/|_|  \___/| .__/ 
+                                    |_|
+                                    
+ Name - RainDrop
+ Version - 1.0.0
+ Creator - RealImNotClippy (or just ImNotClippy, either way) 
+ 
+ This is a simple script thats only 10 lines of code that creates a better wait() (faster than task.wait() or wait())
+-----------------------------------------------------------]]
